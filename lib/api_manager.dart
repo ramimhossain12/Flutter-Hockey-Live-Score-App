7ff34870547af4ca7810ed6169de0fc48853df28
ramimhossain12 <ramimhossain6242@gmail.com>
2021-03-14 21:37:27 +0600

    api call done

--- lib/api_manager.dart
+++ lib/api_manager.dart
@@ -1,18 +1,39 @@
+import 'dart:convert';
+
 import 'package:http/http.dart';
+import 'package:sccore_app/soccermodel.dart';
 
 class SoccerApi {
   final String apiUrl = "https://v3.football.api-sports.io/fixtures?live=all";
 
   //api active key
-  static const api_key = "ea7e05423674ea675f8eeb9a1b49b608";
 
   // api headers add
 
   static const header = {
     'x-rapidapi-host': "v3.football.api-sports.io",
-    'x-rapidapi-key': api_key
-  };
+    'x-rapidapi-key': "ea7e05423674ea675f8eeb9a1b49b608"
+
+};
 
   //create method
 
-}
+  Future<List<SoccerMatch>> getAllMatches() async {
+    Response res = await get(apiUrl, headers: header);
+    var body;
+
+    if (res.statusCode == 200) {
+      // this mean that we are connected to the data base
+      body = jsonDecode(res.body);
+      List<dynamic> matchesList = body['response'];
+      print("Api service: ${body}"); // to debug
+      List<SoccerMatch> matches = matchesList
+          .map((dynamic item) => SoccerMatch.fromJson(item))
+          .toList();
+
+      return matches;
+    }
+  }
+
+  }
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sccore_app/api_manager.dart';
+import 'package:sccore_app/pagerbody.dart';
 
 void main() {
   runApp(MyApp());
@@ -23,20 +25,30 @@ class _SoccerAppState extends State<SoccerApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.orangeAccent,
       appBar: AppBar(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.orangeAccent,
         elevation: 0.0,
         centerTitle: true,
         title: Text(
           "SoccerDashBoard",
-          style: TextStyle(color: Colors.black),
+            style: TextStyle(color: Colors.white),
         ),
       ),
 
       body: FutureBuilder(
-      future: ,
-      builder: ,),
+      future: SoccerApi().getAllMatches(),
+      builder: (context,snapshot){
+
+        if (snapshot.hasData) {
+          print((snapshot.data).length);
+          return PageBody(snapshot.data);
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },),
     );
   }
 }
